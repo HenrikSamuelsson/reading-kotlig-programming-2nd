89a0b3cd2a4b48d90d2017c3f94503941abe5489
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-02-11 11:21:28 +0100

    Remove redundant type definitions.

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
     println("The hero announces her presence to the world")
 
-    val heroName: String = "Madrigal"
+    val heroName = "Madrigal"
     println(heroName)
 
-    var playerLevel: Int = 4
+    var playerLevel = 4
     println(playerLevel)
 
     println("The hero embarks on her journey to locate the enchanted sword.")
