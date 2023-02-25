2059ababa9207257da48e868524eb8796545fa54
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-02-25 09:21:38 +0100

    Make the welcome text yellow.

--- NyetHackOnWorkPC/src/main/kotlin/Main.kt
+++ NyetHackOnWorkPC/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?")
+    narrate("A hero enters the town of Kronstadt. What is their name?", { message ->
+        // Prints the message in yellow
+        "\u001b[33;1m$message\u001b[0m"
+    })
     val heroName = readLine() ?: ""
-
     changeNarratorMood()
     narrate("$heroName heads to the town square")
 }
